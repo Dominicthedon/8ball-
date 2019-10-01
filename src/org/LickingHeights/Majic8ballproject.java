78ac45e29f7f039f68225c3978d5b9f4263747cf
Dominic <dominic.solomon.2022@student.lhschools.org>
2019-10-01 09:41:47 -0400

    Initialized variables

---
+++ src/org/LickingHeights/Majic8ballproject.java
@@ -0,0 +1,60 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Majic8ballproject {
+    public static void main(String[] args) {
+        //code goes here
+        Scanner keyboard;
+        int firstNumber;
+        String question;
+      keyboard = new Scanner(System.in);
+      System.out.println("Please pick a number 1-100");
+firstNumber= keyboard.nextInt();
+if (10 >= firstNumber && firstNumber > 0) {
+    System.out.println("Ask again later");
+}
+if (30 >= firstNumber && firstNumber > 20) {
+System.out.println("Most likely no");
+}
+if (50 >= firstNumber && firstNumber > 40) {
+ System.out.println("As I see it yes");
+}
+if (70 >= firstNumber && firstNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
